65e218e7aa6dfc00174b588306d90eac35cbb9fb
RucasuX <lucasmichelsouza@gmail.com>
2025-01-24 13:05:38 -0300

    Atualizando botao

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ const tg = window.Telegram.WebApp;
 tg.expand(); // Expande o MiniApp para ocupar a tela inteira
 tg.MainButton.text = "Pronto!"; // Define o texto do botão principal
 tg.MainButton.color = "#007bff"; // Define a cor do botão principal
-tg.MainButton.show(); // Exibe o botão principal
+tg.MainButton.show(); // Mostra o botão principal
 
 // Lidar com o clique do botão "btn-action"
 const btnAction = document.getElementById("btn-action");
+const feedback = document.getElementById("feedback");
+
 btnAction.addEventListener("click", () => {
   // Altera o texto do botão principal
   tg.MainButton.text = "Ação realizada!";
+
+  // Exibe uma mensagem na tela
+  feedback.style.display = "block";
 
   // Exibe uma mensagem no console (para depuração)
   console.log("Botão 'Clique aqui' foi clicado!");
